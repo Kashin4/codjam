cf964bf4546e1ac7af6c946346302fc0d1fe69d1
Igor Semak <6399815@mail.ru>
2019-04-14 22:31:04 +0300

    8?

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -1,12 +1,11 @@
-'use strict';
 
-/**************************************************************************************************
+/** ************************************************************************************************
  *                                                                                                *
  * Plese read the following tutorial before implementing tasks:                                   *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
  *                                                                                                *
- **************************************************************************************************/
+ ************************************************************************************************ */
 
 
 /**
@@ -23,7 +22,7 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
 
@@ -38,7 +37,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
 
@@ -54,14 +53,15 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
 
 /**
  * Css selectors builder
  *
- * Each complex selector can consists of type, id, class, attribute, pseudo-class and pseudo-element selectors:
+ * Each complex selector can consists of type, id, class, attribute, pseudo-class and
+ * pseudo-element selectors:
  *
  *    element#id.class[attr]:pseudoClass::pseudoElement
  *              \----/\----/\----------/
@@ -69,9 +69,11 @@ function fromJSON(proto, json) {
  *
  * All types of selectors can be combined using the combinators ' ','+','~','>' .
  *
- * The task is to design a single class, independent classes or classes hierarchy and implement the functionality
+ * The task is to design a single class, independent classes or classes hierarchy and
+ * implement the functionality
  * to build the css selectors using the provided cssSelectorBuilder.
- * Each selector should have the stringify() method to output the string repsentation according to css specification.
+ * Each selector should have the stringify() method to output the string repsentation
+ * according to css specification.
  *
  * Provided cssSelectorBuilder should be used as facade only to create your own classes,
  * for example the first method of cssSelectorBuilder can be like this:
@@ -79,15 +81,18 @@ function fromJSON(proto, json) {
  *       return new MySuperBaseElementSelector(...)...
  *   },
  *
- * The design of class(es) is totally up to you, but try to make it as simple, clear and readable as possible.
+ * The design of class(es) is totally up to you, but try to make it as simple, clear
+ * and readable as possible.
  *
  * @example
  *
  *  var builder = cssSelectorBuilder;
  *
- *  builder.id('main').class('container').class('editable').stringify()  => '#main.container.editable'
+ *  builder.id('main').class('container').class('editable').stringify() =>
+ *    '#main.container.editable'
  *
- *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()  => 'a[href$=".png"]:focus'
+ *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify() =>
+ *    'a[href$=".png"]:focus'
  *
  *  builder.combine(
  *      builder.element('div').id('main').class('container').class('draggable'),
@@ -101,46 +106,46 @@ function fromJSON(proto, json) {
  *               builder.element('td').pseudoClass('nth-of-type(even)')
  *           )
  *      )
- *  ).stringify()        =>    'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
+ *  ).stringify() =>
+ *      'div#main.container.draggable + table#data ~ tr:nth-of-type(even) td:nth-of-type(even)'
  *
  *  For more examples see unit tests.
  */
 
 const cssSelectorBuilder = {
 
-    element: function(value) {
-        throw new Error('Not implemented');
-    },
+  element(value) {
+    throw new Error('Not implemented');
+  },
 
-    id: function(value) {
-        throw new Error('Not implemented');
-    },
+  id(value) {
+    throw new Error('Not implemented');
+  },
 
-    class: function(value) {
-        throw new Error('Not implemented');
-    },
+  class(value) {
+    throw new Error('Not implemented');
+  },
 
-    attr: function(value) {
-        throw new Error('Not implemented');
-    },
+  attr(value) {
+    throw new Error('Not implemented');
+  },
 
-    pseudoClass: function(value) {
-        throw new Error('Not implemented');
-    },
+  pseudoClass(value) {
+    throw new Error('Not implemented');
+  },
 
-    pseudoElement: function(value) {
-        throw new Error('Not implemented');
-    },
+  pseudoElement(value) {
+    throw new Error('Not implemented');
+  },
 
-    combine: function(selector1, combinator, selector2) {
-        throw new Error('Not implemented');
-    },
+  combine(selector1, combinator, selector2) {
+    throw new Error('Not implemented');
+  }
 };
 
-
 module.exports = {
-    Rectangle: Rectangle,
-    getJSON: getJSON,
-    fromJSON: fromJSON,
-    cssSelectorBuilder: cssSelectorBuilder
+  Rectangle: Rectangle,
+  getJSON: getJSON,
+  fromJSON: fromJSON,
+  cssSelectorBuilder: cssSelectorBuilder
 };
